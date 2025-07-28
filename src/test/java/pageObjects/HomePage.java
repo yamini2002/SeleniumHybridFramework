8839dbd5340cd7674b9d7a0f9fcd938abbb77f6e
unknown <yamiikumar@gmail.com>
2025-07-28 23:08:32 +0530

    FirstTestcase

---
+++ src/test/java/pageObjects/HomePage.java
@@ -0,0 +1,34 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage{
+
+	
+	public HomePage(WebDriver driver)
+	{
+		super(driver);
+	}
+	
+@FindBy(xpath="//span[normalize-space()='My Account']") 
+WebElement lnkMyaccount;
+
+@FindBy(xpath="//a[normalize-space()='Register']") 
+WebElement lnkRegister;
+
+
+
+public void clickMyAccount()
+{
+	lnkMyaccount.click();
+}
+
+public void clickRegister()
+{
+	lnkRegister.click();
+}
+
+}
+
