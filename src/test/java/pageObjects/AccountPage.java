4f2eb2b47cf32ab1e8638ce59316790c05b4681a
unknown <yamiikumar@gmail.com>
2025-08-06 20:26:09 +0530

    Links

---
+++ src/test/java/pageObjects/AccountPage.java
@@ -0,0 +1,50 @@
+package pageObjects;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AccountPage extends BasePage {
+
+	public AccountPage(WebDriver driver) {
+		super(driver);
+	}
+
+	@FindBy(xpath = "//div[@id='content']/ul/li/a")
+	List<WebElement> links;
+
+	// Method to return the number of links
+	public int getContentLinksCount() {
+		return links.size();
+	}
+
+	// Method to print all link texts
+	public void printAllLinkTexts() {
+		for (WebElement link : links) {
+			System.out.println(link.getText());
+		}
+	}
+
+	public void openAndVerifyAllLinks() {
+		for (int i = 0; i < links.size(); i++) {
+			// Get the text before clicking (since list might refresh after navigation)
+			String linkText = links.get(i).getText();
+			System.out.println("Clicking link: " + linkText);
+
+			links.get(i).click();
+
+			// Validate page header or title
+			String currentTitle = driver.getTitle();
+			System.out.println("Navigated to page title: " + currentTitle);
+
+			// Navigate back to My Account page
+			driver.navigate().back();
+
+			// Reinitialize elements after navigation
+			PageFactory.initElements(driver, this);
+		}
+	}
+}
---
+++ src/test/java/testCases/TC006_MyAccountTest.java
@@ -0,0 +1,22 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+import pageObjects.AccountPage;
+
+public class TC006_MyAccountTest extends BaseClass {
+
+	@Test
+	public void verify_Links() {
+		TC002_AccountLoginTest at = new TC002_AccountLoginTest();
+		at.verify_login();
+		// Get and print the number of links
+		AccountPage ap = new AccountPage(driver);
+		int linkCount = ap.getContentLinksCount();
+        System.out.println("Total links on My Account page: " + linkCount);
+        // Print the text of each link
+		ap.printAllLinkTexts();
+		// Click and verify each link
+        ap.openAndVerifyAllLinks();
+	}
+}
